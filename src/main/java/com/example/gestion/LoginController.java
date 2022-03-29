abdbcb4d40c7b669ff920deabce5e3ce90262c34
Abdek21 <abdek.housssein@gmail.com>
2022-03-29 21:22:34 +0300

    update

--- src/main/java/com/example/gestion/LoginController.java
+++ src/main/java/com/example/gestion/LoginController.java
@@ -3,13 +3,36 @@ package com.example.gestion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
 public class LoginController {
     @FXML
     private Button cancelbtn;
+    @FXML
+    private Label loginMsg;
 
+    @FXML
+    private TextField usernameTxt;
+    @FXML
+    private PasswordField passwordTxt;
+
+    public void loginButtonOnAction (ActionEvent e){
+
+
+
+
+        if(!usernameTxt.getText().isBlank() && !passwordTxt.getText().isBlank()){
+            loginMsg.setText("Vous voulez vous connecter");
+        }
+        else {
+
+            loginMsg.setText("Username et Password incorrect !");
+        }
+    }
     public void cancelButtonOnAction (ActionEvent e){
 
         Stage stage = (Stage) cancelbtn.getScene().getWindow();
